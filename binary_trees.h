803ae161ed8721e58422166cdfa3ad6a5b299554
ajmarrugos <ajmarrugos@gmail.com>
2022-07-28 23:19:06 -0500

    Size of the tree

--- binary_trees.h
+++ binary_trees.h
@@ -27,11 +27,9 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-/* PRINTING, DELETING & MEASURING THE TREE */
+/* PRINTING & DELETING THE TREE */
 void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
-size_t binary_tree_height(const binary_tree_t *tree);
-size_t binary_tree_depth(const binary_tree_t *tree);
 
 /* NODE INSERTION FUNCTIONS */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -46,5 +44,10 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
+/* MEASURING THE TREE */
+size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
+size_t binary_tree_size(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
