7667e257b00ca3b484d47d93acc76a8c362eafad
ajmarrugos <ajmarrugos@gmail.com>
2022-07-28 23:12:30 -0500

    Depth of the tree

--- binary_trees.h
+++ binary_trees.h
@@ -31,6 +31,7 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 void binary_tree_delete(binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 /* NODE INSERTION FUNCTIONS */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
