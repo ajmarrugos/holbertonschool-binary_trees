c04bdf6438dbecb02aa7f158d998959a23adb43e
ajmarrugos <ajmarrugos@gmail.com>
2022-07-28 22:31:21 -0500

    Closing the endif

--- binary_trees.h
+++ binary_trees.h
@@ -25,3 +25,5 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
+
+#endif /* _BINARY_TREES_H_ */
